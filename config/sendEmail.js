cb762f65d2b800c0a925e85967f645b608f50b71
Shivam Dixit <shivamdixit8780@gmail.com>
2026-08-03 02:51:50 +0530

    Replace Gmail SMTP with Resend email service

--- config/sendEmail.js
+++ config/sendEmail.js
@@ -1,16 +1,10 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to,
     subject,
     text,
